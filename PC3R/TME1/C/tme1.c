4567d8077400d49712146dabc8924c164bc9c5f6
HaotianXUE <Haotian.Xue@etu.sorbonne-universite.fr>
2024-01-25 11:22:06 +0100

    init

---
+++ PC3R/TME1/C/tme1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+
+
+typedef struct paquet
+{
+    char* fruit;
+} paquet;
+
+
+
+typedef struct tapis
+{
+    paquet* fruits;
+    int capacity;
+    
+
+
+
+}tapis;
+
+
+
+
+
+
+
+
+
+
+
+
+
